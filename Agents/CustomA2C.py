c1f635b972f0e4d1e12117842997bc75d34096a7
Hadi Salameh <hads@itu.dk>
2021-02-24 00:41:09 +0100

    initial

---
+++ Agents/CustomA2C.py
@@ -0,0 +1,142 @@
+import random
+import time
+from typing import Union, Type, Optional, Dict, Any, Callable, List
+
+import gym
+import numpy as np
+from stable_baselines3 import A2C
+from stable_baselines3.common.buffers import RolloutBuffer
+from stable_baselines3.common.callbacks import BaseCallback
+from stable_baselines3.common.policies import ActorCriticPolicy
+from stable_baselines3.common.type_aliases import GymEnv
+import torch as th
+from stable_baselines3.common.vec_env import VecEnv
+
+from game.lilys_garden_env import LilysGardenEnv
+
+
+class CustomA2C(A2C):
+    def __init__(self, policy: Union[str, Type[ActorCriticPolicy]],
+                 env: Union[GymEnv, str],
+                 learning_rate: Union[float, Callable] = 7e-4,
+                 n_steps: int = 5,
+                 gamma: float = 0.99,
+                 gae_lambda: float = 1.0,
+                 ent_coef: float = 0.0,
+                 vf_coef: float = 0.5,
+                 max_grad_norm: float = 0.5,
+                 rms_prop_eps: float = 1e-5,
+                 use_rms_prop: bool = True,
+                 use_sde: bool = False,
+                 sde_sample_freq: int = -1,
+                 normalize_advantage: bool = False,
+                 tensorboard_log: Optional[str] = None,
+                 create_eval_env: bool = False,
+                 policy_kwargs: Optional[Dict[str, Any]] = None,
+                 verbose: int = 0,
+                 seed: Optional[int] = None,
+                 device: Union[th.device, str] = "auto",
+                 levels: List[int] = None,
+                 _init_setup_model: bool = True):
+        super(CustomA2C, self).__init__(policy,
+                                        env,
+                                        learning_rate=learning_rate,
+                                        n_steps=n_steps,
+                                        gamma=gamma,
+                                        gae_lambda=gae_lambda,
+                                        ent_coef=ent_coef,
+                                        vf_coef=vf_coef,
+                                        max_grad_norm=max_grad_norm,
+                                        use_sde=use_sde,
+                                        sde_sample_freq=sde_sample_freq,
+                                        tensorboard_log=tensorboard_log,
+                                        policy_kwargs=policy_kwargs,
+                                        verbose=verbose,
+                                        device=device,
+                                        create_eval_env=create_eval_env,
+                                        seed=seed,
+                                        _init_setup_model=_init_setup_model,
+                                        normalize_advantage=normalize_advantage,
+                                        rms_prop_eps=rms_prop_eps,
+                                        use_rms_prop=use_rms_prop)
+        self.levels = levels
+
+    def collect_rollouts(
+            self, env: VecEnv, callback: BaseCallback, rollout_buffer: RolloutBuffer, n_rollout_steps: int
+    ) -> bool:
+        """
+        Collect rollouts using the current policy and fill a `RolloutBuffer`.
+
+        :param n_rollout_steps:
+        :param env: The training environment
+        :param callback: Callback that will be called at each step
+            (and at the beginning and end of the rollout)
+        :param rollout_buffer: Buffer to fill with rollouts
+        :param n_steps: Number of experiences to collect per environment
+        :return: True if function returned with at least `n_rollout_steps`
+            collected, False if callback terminated rollout prematurely.
+        """
+        assert self._last_obs is not None, "No previous observation was provided"
+        n_steps = 0
+        rollout_buffer.reset()
+        # Sample new weights for the state dependent exploration
+        if self.use_sde:
+            self.policy.reset_noise(env.num_envs)
+
+        callback.on_rollout_start()
+
+        while n_steps < n_rollout_steps:
+            if self.use_sde and self.sde_sample_freq > 0 and n_steps % self.sde_sample_freq == 0:
+                # Sample a new noise matrix
+                self.policy.reset_noise(env.num_envs)
+
+            with th.no_grad():
+                # Convert to pytorch tensor
+                obs_tensor = th.as_tensor(self._last_obs).to(self.device)
+                actions, values, log_probs = self.policy.forward(obs_tensor)
+            actions = actions.cpu().numpy()
+
+            # Rescale and perform action
+            clipped_actions = actions
+            # Clip the actions to avoid out of bound error
+            if isinstance(self.action_space, gym.spaces.Box):
+                clipped_actions = np.clip(actions, self.action_space.low, self.action_space.high)
+
+            new_obs, rewards, dones, infos = env.step(clipped_actions)
+
+            # added code here
+            if np.alltrue(dones) and self.levels is not None and isinstance(env, LilysGardenEnv):
+                new_lvl = random.choice(self.levels)
+                env.set_level(new_lvl)
+                env.reset(time.time())
+
+            self.num_timesteps += env.num_envs
+
+            # Give access to local variables
+            callback.update_locals(locals())
+            if callback.on_step() is False:
+                return False
+
+            self._update_info_buffer(infos)
+            n_steps += 1
+
+            if isinstance(self.action_space, gym.spaces.Discrete):
+                # Reshape in case of discrete action
+                actions = actions.reshape(-1, 1)
+            rollout_buffer.add(self._last_obs, actions, rewards, self._last_dones, values, log_probs)
+            self._last_obs = new_obs
+            self._last_dones = dones
+
+        with th.no_grad():
+            # Compute value for the last timestep
+            obs_tensor = th.as_tensor(new_obs).to(self.device)
+            _, values, _ = self.policy.forward(obs_tensor)
+
+        rollout_buffer.compute_returns_and_advantage(last_values=values, dones=dones)
+
+        callback.on_rollout_end()
+
+        return True
+
+    def set_levels(self, levels):
+        self.levels = levels
